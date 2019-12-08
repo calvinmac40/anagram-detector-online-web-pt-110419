4e94d558b3909faf3fe6720dc409790a38d43a90
Calvin McDowell <calvinmac40@hotmail.com>
2019-12-08 12:59:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,16 @@
 require 'pry'
 
-# Your code goes here!
-
 class Anagram
   attr_accessor :word
   
+  @@words = []
+  
   def initialize(word)
     @word = word
+    @@words << self
   end
   
   def self.match(word)
-    array = []
     array.collect do |i|
       i == "#{word}"
       array
